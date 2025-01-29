138cd98da11e91bb62bda2a05ffaffaec401628c
Haasnain Raza <hr1228744@gmail.com>
2025-01-29 17:03:19 +0500

    Add files via upload

---
+++ web development/vedio55/script.js
@@ -0,0 +1,16 @@
+console.log(" hey this is tetorial 55");
+
+let a=5;
+a=a+ 1;
+let b=6;
+let c="hasnian"
+
+// var 66a="ali" noot allowed
+// console.log(a+b)+8;
+// console.log(typeof a,typeof b, typeof c,);
+
+// const a1=6;
+// a1= a1+ = 1;   this is not Allowed because a1 is constant
+{
+console.log(a);
+}
---
+++ web development/vedio56/index.js
@@ -0,0 +1,11 @@
+console.log("hey this is hasnain")
+
+let age = 17;
+let grace = 2;
+
+if (age + grace >= 18){ 
+    console.log('you can drive');
+}
+
+else 
+    console.log("you canot drive");
